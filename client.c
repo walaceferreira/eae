8a19c3326881357b21a3e391eb983892dc2f1c27
Knushh <channeltvtutors@gmail.com>
2018-12-05 07:57:23 -0500

    Create client.c

---
+++ client.c
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pwned</title>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Lora:700i" rel="stylesheet"> 
+	<style type="text/css">
+		
+	body{
+    
+    background-image: url(http://www.wheretowillie.com/wp-content/uploads/2013/03/The-Blues-Fall-on-Yosemite.jpg);
+    }
+    
+      h1{
+      	font-family: 'Lora', serif;
+      	font-size-adjust: 1; 
+      }
+
+     h1{
+     	font-family: 'Lora', serif;
+     }
+	</style>
+</head>
+<body>
+
+	<center>
+		<h1>Interditado por : #Knushh</h1>
+		<img src="https://png2.kisspng.com/sh/ddf75df836786e7dcdb4fbfbfb60617f/L0KzQYm3VsMxN5N9g5H0aYP2gLBuTfhie5l5edk2eXB4hMbphb1lcZJshtH8dHnmg368gsM5OJM2T6RsNXa6QHAAV8I6OmY5TqMAM0C2SYq8UcEyQGI9RuJ3Zx==/kisspng-hashtag-youtube-diagnostics-5b380b172c5f70.7729254615303995111818.png" height="300">
+		<h2>É patético ver pessoas que não entendem da área ditando as regras de um artigo,O Artigo 13 Vai quebrar a internet e vai junto economia dos paises que adotarem o artigo #DeleteArt13 Proteste #saveyourinternet</h2>
+	</center>
+
+</body>
+</html>
